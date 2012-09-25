18261f3807cc994374d01973953919fecef1f1a9
Niclas <Niclas@Niclas.(none)>
2012-09-25 20:29:20 +0200

    Städat lite i koden

--- src/kvhc/adrumdrum/MainActivity.java
+++ src/kvhc/adrumdrum/MainActivity.java
@@ -1,5 +1,6 @@
 package kvhc.adrumdrum;
 
+import kvhc.gui.GUIController;
 import kvhc.player.Player;
 import kvhc.util.AndroidTimer;
 import android.os.Bundle;
@@ -18,11 +19,11 @@ import android.widget.CheckBox;
 
 public class MainActivity extends Activity {
 	
-	Runnable r;
-	AndroidTimer mTimer;
+	
 	TextView tv1;
 	RadioGroup activeSteps;
 	private Player player;
+	private GUIController guic;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -30,13 +31,10 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         tv1 = (TextView)findViewById(R.id.textView1);
         activeSteps = (RadioGroup)findViewById(R.id.radiogroup);
-       
-        initBars();
-
+        
         player = new Player(getBaseContext(), this);
-    	
-        Button btn1 = (Button)findViewById(R.id.button1);
-        btn1.setOnClickListener(btnListener);
+        guic = new GUIController(player, this);
+    
     }
     
     public void onCheckboxClicked(View view) {
@@ -72,80 +70,9 @@ public class MainActivity extends Activity {
 
     }
     
-    
-    private OnClickListener btnListener = new OnClickListener()
-    {
-		public void onClick(View v) {
-			if(player.IsPlaying()) {
-    			player.Stop();
-    			tv1.setText("Not playing anymore");
-    			
-    		} else {
-    			player.Play();
-    			tv1.setText("Playing");
-    		}
-		}
-    };
-    
-    
-    
-    private void initBars(){
-    	SeekBar bpmBar = (SeekBar)findViewById(R.id.bpmbar);
-    	SeekBar panningBar = (SeekBar)findViewById(R.id.panningbar);
-    	TextView bpmtext = (TextView) findViewById(R.id.bpmtext);
-    	
-    	panningBar.setProgress(50);
-    	
-    	bpmBar.setOnSeekBarChangeListener( new SeekBar.OnSeekBarChangeListener() {
 
-			//@Override
-			public void onProgressChanged(SeekBar arg0, int progress, boolean arg2) {
-				player.setBPMInRange(progress);
-				tv1.setText("BPM is: " + (60 + progress*6));
-				
-			}
-
-			//@Override
-			public void onStartTrackingTouch(SeekBar arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			//@Override
-			public void onStopTrackingTouch(SeekBar arg0) {
-				//TODO
-				
-			}
-        });
-    	
-    	panningBar.setOnSeekBarChangeListener( new SeekBar.OnSeekBarChangeListener() {
-
-			//@Override
-			public void onProgressChanged(SeekBar arg0, int progress, boolean arg2) {
-				if(progress >= 50){					
-					player.panning(1.0, 1.0 - ((progress - 50) * 0.02));
-					tv1.setText("Panned: R=1.0 L=" + (1.0 -(progress - 50) * 0.02));
-				}
-				else if (progress < 50){
-					player.panning(1.0 - (50 - progress) * 0.02, 1.0);
-					tv1.setText("Panned: R=" +((1.0 - (50 - progress) * 0.02)) + "L=1.0");
-				}
-
-				
-			}
-
-			//@Override
-			public void onStartTrackingTouch(SeekBar arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-
-			//@Override
-			public void onStopTrackingTouch(SeekBar arg0) {
-				//TODO
-				
-			}
-        });
+    public Object getFromR(int id){
+    	return findViewById(id);
     }
     
     public void setRadioButtonToActiveStep(int n) {
--- src/kvhc/gui/GUIController.java
+++ src/kvhc/gui/GUIController.java
@@ -1,5 +1,122 @@
 package kvhc.gui;
 
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import kvhc.adrumdrum.MainActivity;
+import kvhc.adrumdrum.R;
+import kvhc.player.Player;
+
 public class GUIController {
 
+	
+	private Player player;
+	private MainActivity ma;
+	private TextView tv1;
+	
+	public GUIController(Player player, MainActivity ma){
+		this.player = player;
+		this.ma = ma;
+		
+		init();
+	}
+	
+	private void init(){
+		initText();
+		initButtons();
+		initBars();
+	}
+	
+	
+	
+	
+	
+    private void initText() {
+    	tv1 = (TextView)ma.getFromR(R.id.textView1);
+		
+	}
+
+	private void initButtons() {
+    	   Button btn1 = (Button)ma.getFromR(R.id.button1);
+           btn1.setOnClickListener(btnListener);
+		
+	}
+
+	private void initBars(){
+    	SeekBar bpmBar = (SeekBar)ma.getFromR(R.id.bpmbar);
+    	SeekBar panningBar = (SeekBar)ma.getFromR(R.id.panningbar);
+    	TextView bpmtext = (TextView) ma.getFromR(R.id.bpmtext);
+    	
+    	panningBar.setProgress(50);
+    	
+    	bpmBar.setOnSeekBarChangeListener( new SeekBar.OnSeekBarChangeListener() {
+
+			//@Override
+			public void onProgressChanged(SeekBar arg0, int progress, boolean arg2) {
+				player.setBPMInRange(progress);
+				tv1.setText("BPM is: " + (60 + progress*6));
+				
+			}
+
+			//@Override
+			public void onStartTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			//@Override
+			public void onStopTrackingTouch(SeekBar arg0) {
+				//TODO
+				
+			}
+        });
+    	
+    	panningBar.setOnSeekBarChangeListener( new SeekBar.OnSeekBarChangeListener() {
+
+			//@Override
+			public void onProgressChanged(SeekBar arg0, int progress, boolean arg2) {
+				if(progress >= 50){					
+					player.panning(1.0, 1.0 - ((progress - 50) * 0.02));
+					tv1.setText("Panned: R=1.0 L=" + (1.0 -(progress - 50) * 0.02));
+				}
+				else if (progress < 50){
+					player.panning(1.0 - (50 - progress) * 0.02, 1.0);
+					tv1.setText("Panned: R=" +((1.0 - (50 - progress) * 0.02)) + "L=1.0");
+				}
+
+				
+			}
+
+			//@Override
+			public void onStartTrackingTouch(SeekBar arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			//@Override
+			public void onStopTrackingTouch(SeekBar arg0) {
+				//TODO
+				
+			}
+        });
+    }
+	
+	
+    private OnClickListener btnListener = new OnClickListener()
+    {
+		public void onClick(View v) {
+			if(player.IsPlaying()) {
+    			player.Stop();
+    			tv1.setText("Not playing anymore");
+    			
+    		} else {
+    			player.Play();
+    			tv1.setText("Playing");
+    		}
+		}
+    };
+    
+	
 }
