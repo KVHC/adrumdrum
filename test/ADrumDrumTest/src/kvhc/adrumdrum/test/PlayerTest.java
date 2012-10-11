e2d75c945b82272e1849ccdbf7a517e22fe86dac
danfal <danfal@student.chalmers.se>
2012-10-11 22:43:46 +0200

    Did some more work on the tests

--- test/ADrumDrumTest/src/kvhc/adrumdrum/test/PlayerTest.java
+++ test/ADrumDrumTest/src/kvhc/adrumdrum/test/PlayerTest.java
@@ -33,13 +33,14 @@ public class PlayerTest extends AndroidTestCase {
 		TestObserver testObserver = new TestObserver();
 		testPlayer.addObserver(testObserver);
 		testPlayer.Play();
+		
 		// I don't know why this keeps failing,
 		// but I think the test is broken, perhaps
 		// I'm not initiating the class correctly
 		try{
-			Thread.sleep(10000);
+			Thread.sleep(3000);
 		}catch(Exception e){
-			Assert.fail("Thread.sleep() threw exception");
+			Assert.fail("Thread.sleep() threw exception in PlayerTest");
 		}
 		Assert.assertTrue(testObserver.getCount()>0);
 		
--- test/ADrumDrumTest/src/kvhc/adrumdrum/test/TestObserver.java
+++ test/ADrumDrumTest/src/kvhc/adrumdrum/test/TestObserver.java
@@ -11,7 +11,7 @@ import java.util.Observer;
  */
 public class TestObserver implements Observer {
 	
-	int count;
+	private int count;
 	
 	public TestObserver(){
 		//super();
