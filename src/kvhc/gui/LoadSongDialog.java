1f205561a03f9fa60094607e1fd3fb7b19a7bf2c
Oscar <oscar.dragen@gmail.com>
2012-10-13 21:10:43 +0200

    Bit of work on the loader
    Yeah mu

--- src/kvhc/gui/LoadSongDialog.java
+++ src/kvhc/gui/LoadSongDialog.java
@@ -8,7 +8,12 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * The Load Song dialog shows a list of saved songs (currently from the db)
@@ -46,7 +51,27 @@ public class LoadSongDialog extends Dialog {
 		setTitle("Load Song");
 		
 		// Set up the list
+		ListView songList = (ListView)findViewById(R.id.savedSongsList);
 		
+		String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+				  "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+				  "Linux", "OS/2" };
+		
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(),
+				  R.layout.song_list_item, R.id.savedSongsList, values);
+
+		songList.setAdapter(adapter);
+		
+		songList.setOnItemClickListener(new OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> parent, View view, int position,
+					long id) {
+				Toast.makeText(getContext(),
+					      "Click ListItem Number " + position, Toast.LENGTH_LONG)
+					      .show();
+				
+			}
+		});
 		
 		// Set up load button
 		Button b = (Button)findViewById(R.id.btnLoadSong);
