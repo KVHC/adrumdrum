c64e4f48b39764be9ab7e20aea999b8e2a996dee
Oscar <oscar.dragen@gmail.com>
2012-10-14 04:26:55 +0200

    Saving and loading update checkpoint
    updateable rows and insertable rows functions things

--- src/kvhc/util/SQLSongLoader.java
+++ src/kvhc/util/SQLSongLoader.java
@@ -34,9 +34,7 @@ public class SQLSongLoader implements ISongLoader {
 		if(args.length != 1) {
 			return null;
 		}
-		
-		
-		
+
 		if(args[0].getClass() != String.class) {
 			// if source is not a string,
 			// we cant use it
@@ -59,7 +57,7 @@ public class SQLSongLoader implements ISongLoader {
 	private ArrayList<Sound> getSoundList() {
 		
 		if(mSoundList == null) {
-			
+			// Cache the sound list
 			Cursor soundCursor = mDB.getCursorForTable(DatabaseHandler.TABLE_CHANNEL
 					, new String[] { DatabaseHandler.KEY_ID, DatabaseHandler.KEY_SOUNDVALUE, DatabaseHandler.KEY_NAME, 
 					DatabaseHandler.KEY_RIGHTPAN, DatabaseHandler.KEY_LEFTPAN },
@@ -67,33 +65,41 @@ public class SQLSongLoader implements ISongLoader {
 					new String[] {});
 			
 			if(soundCursor.moveToFirst()) {
+				// There are some sounds
 				
 				Sound sound = null;
-				
 				mSoundList = new ArrayList<Sound>(soundCursor.getCount());
 				
 				do {
+					// Get sound info from db
 					int id = soundCursor.getInt(0);
 					int soundValue = soundCursor.getInt(1);
 					String name = soundCursor.getString(2);
 					
+					// Create sound
 					sound = new Sound(id, soundValue, name);
 					
+					// Add to sound list (id = primary key start from 1, arraylist index from 0 i think)
 					mSoundList.add(id-1, sound);
 					
 				} while(soundCursor.moveToNext());
 			}
-			
-			if(mSoundList == null) 
-				mSoundList = new ArrayList<Sound>(0); 
 		}
+		
+		if(mSoundList == null) 
+			mSoundList = new ArrayList<Sound>(0);
 		
 		return mSoundList;
 	}
 	
+	
+	/**
+	 * Creates an list for the steps of a channel
+	 * @param channelId the id of the channel 
+	 * @return a list of steps 
+	 */
 	private ArrayList<Step> getStepsForChannel(int channelId) {
-		
-		
+
 		ArrayList<Step> steps = null;
 		
 		Cursor stepCursor = mDB.getCursorForTable(DatabaseHandler.TABLE_STEP,
@@ -109,7 +115,8 @@ public class SQLSongLoader implements ISongLoader {
 			
 			do {
 				// Load the parameters
-				int id = stepCursor.getInt(0);
+				// TODO check which variables are used
+				int id = stepCursor.getInt(0); 
 				int number = stepCursor.getInt(1);
 				double velocity = stepCursor.getDouble(2);
 				boolean active = stepCursor.getInt(3) != 0;
@@ -133,13 +140,19 @@ public class SQLSongLoader implements ISongLoader {
 		return steps;
 	}
 
+	/**
+	 * Gets the channels for a song from the database
+	 * @param songId - the id of the song
+	 * @return a list of channels
+	 */
 	private ArrayList<Channel> getChannelsForSongId(int songId) {
 		
 		ArrayList<Channel> channels = null;
 		
 		Cursor channelCursor = mDB.getCursorForTable(DatabaseHandler.TABLE_CHANNEL
 				, new String[] { DatabaseHandler.KEY_ID, DatabaseHandler.KEY_VOLUME, 
-				DatabaseHandler.KEY_RIGHTPAN, DatabaseHandler.KEY_LEFTPAN, DatabaseHandler.FKEY_SOUNDID },
+				DatabaseHandler.KEY_RIGHTPAN, DatabaseHandler.KEY_LEFTPAN, DatabaseHandler.FKEY_SOUNDID,
+				DatabaseHandler.KEY_NUMBER },
 				DatabaseHandler.FKEY_SONGID + "=?", 
 				new String[] { String.valueOf(songId) });
 		
@@ -150,14 +163,18 @@ public class SQLSongLoader implements ISongLoader {
 			channels = new ArrayList<Channel>(channelCursor.getCount());
 			
 			do {
+				// Get song settings
 				int id = channelCursor.getInt(0);
 				double volume = channelCursor.getDouble(1);
 				double rightPan = channelCursor.getDouble(2);
 				double leftPan = channelCursor.getDouble(3);
 				int soundId = channelCursor.getInt(4);
+				int number = channelCursor.getInt(5);
 				
+				// Set sound
 				Sound sound = getSoundList().get(soundId-1);
 				
+				// Set the steps
 				ArrayList<Step> steps = getStepsForChannel(id);
 				
 				channel = new Channel(sound, steps.size());
@@ -165,10 +182,12 @@ public class SQLSongLoader implements ISongLoader {
 					channel.setStep(step.getStepNumber(), step.isActive(), step.getVelocity());
 				}
 				
+				// Set volume and panning
 				channel.setVolume((float)volume);
 				channel.setPanning((float)rightPan, (float)leftPan);
 				
-				channels.add(channel);
+				// add to channel list
+				channels.add(number, channel);
 			} while(channelCursor.moveToNext());
 		} else {
 			// There are no channels
@@ -185,7 +204,7 @@ public class SQLSongLoader implements ISongLoader {
 	 */
 	public ArrayList<Song> getSongList(Object[] args) {
 		
-		ArrayList<Song> songs;
+		ArrayList<Song> songs = null;
 		
 		Cursor songCursor = mDB.getCursorForTable(DatabaseHandler.TABLE_SONG, new String[] {DatabaseHandler.KEY_ID , DatabaseHandler.KEY_NAME }, "", null );
 		
@@ -202,13 +221,15 @@ public class SQLSongLoader implements ISongLoader {
 				String songName = songCursor.getString(1);
 				
 				// Load the channels
-				
-				
-				
+				song = new Song(getChannelsForSongId(songId));
 				
 				songs.add(song);
 				
 			} while(songCursor.moveToNext());
+		}
+		
+		if(songs == null) {
+			songs = new ArrayList<Song>(0);
 		}
 		
 		return songs;
