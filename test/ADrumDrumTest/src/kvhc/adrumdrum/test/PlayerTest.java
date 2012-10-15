2092bbbc69be9b6652be90e5227fca04146df177
danfal <danfal@student.chalmers.se>
2012-10-15 18:24:47 +0200

    fixed PlayerTest

--- test/ADrumDrumTest/src/kvhc/adrumdrum/test/PlayerTest.java
+++ test/ADrumDrumTest/src/kvhc/adrumdrum/test/PlayerTest.java
@@ -23,33 +23,37 @@ public class PlayerTest extends AndroidTestCase implements Observer {
 	public void testConstructor (){
 		Player testPlayer = new Player(getContext());
 		Assert.assertNotNull(testPlayer);
+		Assert.assertFalse(testPlayer.isPlaying());
 	}
 	
 	/**
 	 * Asserts that when the play-method is called the song plays
-	 * 
+	 * and also checks that stopping notifies observers.
 	 */
-	public void testPlay(){
+	public void testPlayAndStop(){
+		//init
 		count=0;
 		Player testPlayer = new Player(getContext());
 		ArrayList<Channel> testList = new ArrayList<Channel>();
 		testList.add(new Channel(new Sound(1,1,"test"),1));
 		testPlayer.addObserver(this);
+		
 		testPlayer.play();
-		
-		// I don't know why this keeps failing,
-		// but I think the test is broken, perhaps
-		// I'm not initiating the class correctly
-		try{
-			Thread.sleep(3000);
-		} catch(Exception e){
-			Assert.fail("Thread.sleep() threw exception in PlayerTest");
-		}
+		Assert.assertTrue(testPlayer.isPlaying());
+		//stopping the player should notify observers
+		testPlayer.stop();
+		//if update has been called this should be true
 		Assert.assertTrue(count>0);
-		
-		Assert.assertEquals(testPlayer.isPlaying(),true);
+		Assert.assertFalse(testPlayer.isPlaying());
+		//after stopping, current step should be 0
+		Assert.assertEquals(0, testPlayer.getActiveStep());
 	}
 
+	
+	/**
+	 * Implementation of the update function from the Observable interface
+	 * used to assert that Player correctly notifies its observers.
+	 */
 	public void update(Observable observable, Object data) {
 		count++;
 		
